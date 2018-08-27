1de88480e04fc7643789b5cc0df588fd4d666022
DeliriousLee <1027959757@outlook.com>
2018-08-27 16:10:27 +0800

    Update HZSFPictureToolViewController.m

--- HZSFPictureToolViewController.m
+++ HZSFPictureToolViewController.m
@@ -7,38 +7,27 @@
 //
 
 #import "HZSFPictureToolViewController.h"
-#import "UINavigationController+NTESNavbarShadow.h"
-#import "UIImage+HZSFImageCompressTool.h"
-#import "UIViewController+HZSFSetTitle.h"
+
 #define MaxScale 4.0
 #define MinScale 0.7
 
 @interface HZSFPictureToolViewController()<UIScrollViewDelegate>
-//@property(nonatomic,assign) CGFloat totalScale;
-@property(nonatomic,strong)UILabel *Naviitem;
-@property(nonatomic,strong)UITapGestureRecognizer *clicked;
+
 @end
 
 @implementation HZSFPictureToolViewController
-//-(void)viewWillAppear:(BOOL)animated{
-//    [super viewWillAppear:animated];
-//
-//    [self.navigationController setNavigationBarHidden:YES animated:animated];
-//}
+
 //使用scrollView本身自带的放大缩小代理
 -(void)viewDidLoad{
     [super viewDidLoad];
-//    _scrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(0,56, _showImage.size.width, _showImage.size.height)];
 
-//    [self.view addSubview:_scrollView];
     [self setViewControllerTitle:@"查看图片"];
     CGFloat imageWidth=_showImage.size.width;
     CGFloat imageHeight=_showImage.size.height;
     
     CGFloat imageRatio=imageWidth/imageHeight;
     
-//    CGFloat screenWidth=_scrollView.frame.size.width;
-//    CGFloat screenHeight=_scrollView.frame.size.height;
+
     CGFloat screenWidth=_scrollView.bounds.size.width;
     CGFloat screenHeight=_scrollView.bounds.size.height;
    
@@ -63,8 +52,6 @@
 
     _scrollView.contentSize=_showImage.size;
     
-//    _imageView.center=CGPointMake(self.navigationController.navigationBar.center.x, self.view.center.y);//self.view或者scrollview的中心点都不准，用了导航栏的中心点来纠正图片开始往左偏的情况（往左偏，大概是采用缩放的时候使用了setRecMake函数）
-//    _imageView.center=CGPointMake(self.navigationController.navigationBar.center.x, [UIScreen mainScreen].bounds.size.height/2);//self.view或者scrollview的中心点都不准，用了导航栏的中心点来纠正图片开始往左偏的情况（往左偏，大概是采用缩放的时候使用了setRecMake函数）
     _imageView.center=CGPointMake(self.navigationController.navigationBar.center.x, self.view.bounds.size.height/2);//self.view或者scrollview的中心点都不准，用了导航栏的中心点来纠正图片开始往左偏的情况（往左偏，大概是采用缩放的时候使用了setRecMake函数）
     
     _scrollView.delegate= self;
@@ -100,137 +87,6 @@
 -(BOOL)prefersStatusBarHidden{
     return YES;
 }
-
-
-
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//
-//    _imageView=[[UIImageView alloc]init];
-//
-//    [_imageView setMultipleTouchEnabled:YES];
-//
-//    [_imageView setUserInteractionEnabled:YES];
-//
-//    [_imageView setImage:_showImage];
-//
-//
-//
-//
-//    self.totalScale=1.0;
-//
-//    [self addGestureRecognizerToView:_imageView];
-//
-//    [self.view addSubview:_imageView];
-//
-//    CGFloat width=_showImage.size.width;
-//    CGFloat height=_showImage.size.height;
-//
-//    while (_imageView.frame.size.height>self.view.frame.size.height||_imageView.frame.size.width>self.view.frame.size.width) {
-//
-//        [_imageView setFrame:CGRectMake((self.view.frame.size.width-_imageView.frame.size.width)/2, (self.view.frame.size.height-_imageView.frame.size.height)/2, width/2, height/2)];
-//        width=width/2;
-//        height=height/2;
-//    }
-////    [_imageView setFrame:CGRectMake((self.view.frame.size.width-_showImage.size.width)/2, (self.view.frame.size.height-_showImage.size.height)/2, _showImage.size.width, _showImage.size.height)];
-//}
-//// 添加所有的手势
-//
-//
-//-(void) addGestureRecognizerToView:(UIView *)view
-//
-//{
-//
-//    // 旋转手势
-//
-//    UIRotationGestureRecognizer *rotationGestureRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotateView:)];
-//
-//    [view addGestureRecognizer:rotationGestureRecognizer];
-//
-//    // 缩放手势
-//
-//    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchView:)];
-//
-//    [view addGestureRecognizer:pinchGestureRecognizer];
-//
-//    // 移动手势
-//
-//    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panView:)];
-//
-//    [view addGestureRecognizer:panGestureRecognizer];
-//
-//    }
-//
-//// 处理旋转手势
-//-(void)rotateView:(UIRotationGestureRecognizer *)rotationGestureRecognizer{
-//
-//    UIView *view = rotationGestureRecognizer.view;
-//
-//    if(rotationGestureRecognizer.state == UIGestureRecognizerStateBegan || rotationGestureRecognizer.state == UIGestureRecognizerStateChanged)
-//
-//    {
-//
-//        view.transform = CGAffineTransformRotate(view.transform, rotationGestureRecognizer.rotation);
-//
-//        [rotationGestureRecognizer setRotation:0];
-//
-//        }
-//
-//}
-//
-//
-//// 处理拖拉手势
-//
-//-(void) panView:(UIPanGestureRecognizer *)panGestureRecognizer
-//
-//{
-//
-//    UIView *view = panGestureRecognizer.view;
-//
-//    if(panGestureRecognizer.state == UIGestureRecognizerStateBegan || panGestureRecognizer.state == UIGestureRecognizerStateChanged)
-//
-//        {
-//
-//            CGPoint translation = [panGestureRecognizer translationInView:view.superview];
-//
-//            [view setCenter:(CGPoint){view.center.x + translation.x, view.center.y + translation.y}];
-//
-//            [panGestureRecognizer setTranslation:CGPointZero inView:view.superview];
-//
-//        }
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//// 处理缩放手势
-//-(void)pinchView:(UIPinchGestureRecognizer *)pinchGestureRecognizer{
-//
-//    CGFloat scale=pinchGestureRecognizer.scale;
-//    if(scale >1.0){
-//        if(_totalScale>MaxScale){
-//            return;
-//        }
-//    }
-//    if(scale <1.0){
-//        if(_totalScale<MinScale){
-//            return;
-//        }
-//    }
-//    self.imageView.transform=CGAffineTransformScale(self.imageView.transform, scale, scale);
-//    self.totalScale *=scale;
-//    pinchGestureRecognizer.scale=1.0;
-//
-//}
-//
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
 
 
 @end
